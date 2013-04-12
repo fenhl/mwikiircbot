9dc7b2e50294871595ee11d026f5329dcf41a8f1
Fenhl <fenhl@fenhl.net>
2013-04-12 11:29:36 +0000

    Add actual recent changes listener

--- mwikiircbot.py
+++ mwikiircbot.py
@@ -1,5 +1,21 @@
 import ircbotframe
+import socketserver
 import sys
+
+class RecentChangesServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
+    pass
+
+class RecentChangesHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        try:
+            line = self.request[0]
+        except:
+            return
+        if not line:
+            return
+        line = line.decode("utf-8").rstrip("\r\n")
+        for chan in handler.channels:
+            handler.bot.say(chan, line)
 
 class Handler:
     def __init__(self, host, port=6667, name="MediaWiki", description="MediaWiki recent changes bot", channels=[]):
@@ -37,7 +53,10 @@ def main(cmd, args):
         print("Usage: " + cmd + " [-n <name>] <host> <channel> [<channel> ...]")
         return
     else:
-        Handler(host=host, name=name, channels=channels)
+        global handler
+        handler = Handler(host=host, name=name, channels=channels)
+        rcserver = RecentChangesServer(("localhost", 51666), RecentChangesHandler)
+        rcserver.serve_forever()
 
 if __name__ == "__main__":
     main(sys.argv[0], sys.argv[1:] if len(sys.argv) > 1 else [])

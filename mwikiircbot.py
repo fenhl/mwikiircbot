095315bfe5ee8982e98104b231432bc27ef5af34
Fenhl <fenhl@fenhl.net>
2013-10-22 11:10:41 +0000

    Server password command-line option

--- mwikiircbot.py
+++ mwikiircbot.py
@@ -3,15 +3,16 @@
 
 Usage:
   mwikiircbot.py [options] <host> <channel>...
-  
+
 Options:
-  -h, --help         Print this message and exit.
-  -n, --name=<name>  Use this name as the IRC nick [Default: MediaWiki].
-  --port=<port>      Connect to the IRC server at this port [Default: 6667].
-  --version          Print version info and exit.
+  -h, --help                    Print this message and exit.
+  -n, --name=<name>             Use this name as the IRC nick [Default: MediaWiki].
+  --server-password=<password>  Use this server password.
+  --port=<port>                 Connect to the IRC server at this port [Default: 6667].
+  --version                     Print version info and exit.
 """
 
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 from docopt import docopt
 import ircbotframe
@@ -34,10 +35,10 @@ class RecentChangesHandler(socketserver.BaseRequestHandler):
             handler.bot.say(chan, line)
 
 class Handler:
-    def __init__(self, host, port=6667, name="MediaWiki", description="MediaWiki recent changes bot", channels=[]):
+    def __init__(self, host, port=6667, name='MediaWiki', description='MediaWiki recent changes bot', channels=[], server_pass=None):
         self.channels = channels
-        self.bot = ircbotframe.ircBot(host, port, name, description)
-        self.bot.bind("376", self.endMOTD)
+        self.bot = ircbotframe.ircBot(host, port, name, description, password=server_pass)
+        self.bot.bind('376', self.endMOTD)
         self.bot.start()
     
     def endMOTD(self, sender, headers, message):
@@ -47,7 +48,7 @@ class Handler:
 def main(arguments):
     channels = arguments['<channel>']
     global handler
-    handler = Handler(host=arguments['<host>'], port=int(arguments['--port']), name=arguments['--name'], channels=arguments['<channel>'])
+    handler = Handler(host=arguments['<host>'], port=int(arguments['--port']), name=arguments['--name'], channels=arguments['<channel>'], server_pass=arguments['--server-password'])
     rcserver = RecentChangesServer(('localhost', 51666), RecentChangesHandler)
     rcserver.serve_forever()
 
